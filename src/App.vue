35f8693e9a81fdf7ec9b61f785c7318d2d6e03ac
JP Lovett <50369339+jplovett@users.noreply.github.com>
2025-11-01 09:50:06 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <main class="container mt-4">
+      <h1>{{ msg }}</h1>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return { msg: 'Hello from App.vue - msg data property' }
+  }
+}
+</script>
+
+<style>
+/* optional global styles for the component */
+#app {
+  font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", Arial;
+}
+</style>
